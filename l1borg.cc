9ed6f3d7c7176f0cc4e0c552f73b20408f9b59e6
Volnei C Puttini <vcputtini@gmail.com>
2021-04-01 10:02:47 -0300

    New simplified commands added: --mount/--umount

--- l1borg.cc
+++ l1borg.cc
@@ -14,6 +14,9 @@ void help()
       << "              NOTE: Enter only the repository name without the path: e.g: --list R1 | --list R1::archive" << std::endl
       << "--diff        : REPONAME::ARCHIVE1 ARCHIVE2" << std::endl
       << "              NOTE: Enter only the repository name without the path" << std::endl
+      << "--[u]mount    : Mount/umount an archive from a repository" << std::endl
+      << "              --mount R1::ARCHIVE MOUNTPOINT" << std::endl
+      << "              --umount MOUNTPOINT" << std::endl
       << "--help        : See this help." << std::endl << std::endl
       << "l1borg.xml    : The possible installation folders for the l1borg.xml file are:" << std::endl
       << "                /etc | /etc/borg | /etc/l1borg | same folder as l1borg executable." << std::endl;
@@ -31,6 +34,8 @@ int main(int argc, char **argv)
     {"info",        required_argument,  0, 'd'},
     {"list",        required_argument,  0, 'e'},
     {"diff",        required_argument,  0, 'f'},
+    {"mount",       required_argument,  0, 'g'},
+    {"umount",      required_argument,  0, 'h'},
     {"help",        no_argument,        0, 'w'},
     {0,             0,                  0, 0}
   };
@@ -43,6 +48,11 @@ int main(int argc, char **argv)
       std::cout << sv;
       exit(EXIT_FAILURE);
   }
+
+  /*
+   * If there are errors in the passed arguments, the default error messages
+   * from BorgBackup will be displayed.
+   */
 
   while ((opt=getopt_long_only(argc,argv,"",long_options, &long_index)) != -1) {
     switch(opt) {
@@ -72,9 +82,13 @@ int main(int argc, char **argv)
       case 'f':
         EnvRepos::get().ExecAlias("diff", std::string(argv[2]), std::string(argv[3]));
         break;
-      case 'w':
-        help();
+      case 'g':
+        EnvRepos::get().ExecAlias("mount", std::string(argv[2]), std::string(argv[3]));
         break;
+      case 'h':
+        EnvRepos::get().ExecAlias("umount", std::string(argv[2]));
+        break;
+      case 'w':
       default:
         help();
         exit(EXIT_SUCCESS);
